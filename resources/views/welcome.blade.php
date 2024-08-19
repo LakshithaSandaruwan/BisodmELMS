d5dc86dcde779de2f1ea1f94fa9691c36f783358
MalshanH <malshanhd@yahoo.com>
2024-08-19 12:38:08 +0530

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
     <link href="assets/img/favicon.png" rel="icon">
     <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
-   
+
 </head>
 
 <body class="index-page">
@@ -61,7 +61,8 @@
                                     <div class="icon-box" data-aos="zoom-out" data-aos-delay="300">
                                         <i class="bi bi-gem"></i>
                                         <h4>Vision</h4>
-                                        <p>Preparing wise children enriched with good qualities that surpass the current competition</p>
+                                        <p>Preparing wise children enriched with good qualities that surpass the current
+                                            competition</p>
                                     </div>
                                 </div><!-- End Icon Box -->
 
@@ -69,7 +70,11 @@
                                     <div class="icon-box" data-aos="zoom-out" data-aos-delay="400">
                                         <i class="bi bi-clipboard-data"></i>
                                         <h4>Mission</h4>
-                                        <p>As an home school program, Bisdom Academy employs a standards-based curriculum that caters to individual differences and learning preferences. We encourage students to take responsibility of their current and future education by developing their academic, social, self-awareness, and technological abilities.</p>
+                                        <p>As an home school program, Bisdom Academy employs a standards-based
+                                            curriculum that caters to individual differences and learning preferences.
+                                            We encourage students to take responsibility of their current and future
+                                            education by developing their academic, social, self-awareness, and
+                                            technological abilities.</p>
                                     </div>
                                 </div><!-- End Icon Box -->
 
@@ -77,7 +82,8 @@
                                     <div class="icon-box" data-aos="zoom-out" data-aos-delay="500">
                                         <i class="bi bi-balloon-heart"></i>
                                         <h4>Values</h4>
-                                        <p>We mainly focus on values of learing, Relationship, Integrity, Accoutability, Innovation and Respect</p>
+                                        <p>We mainly focus on values of learing, Relationship, Integrity, Accoutability,
+                                            Innovation and Respect</p>
                                     </div>
                                 </div><!-- End Icon Box -->
 
@@ -89,6 +95,180 @@
             </div>
 
         </section>
+
+        <!-- Testimonials Section -->
+        <section id="testimonials" class="testimonials section">
+
+            <div class="container">
+
+                <div class="row align-items-center">
+
+                    <div class="col-lg-5 info" data-aos="fade-up" data-aos-delay="100">
+                        <h3>Testimonials</h3>
+                        <p>
+                            Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                            reprehenderit in voluptate
+                            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                            proident.
+                        </p>
+                    </div>
+
+                    <div class="col-lg-7" data-aos="fade-up" data-aos-delay="200">
+
+                        <div class="swiper init-swiper">
+                            <script type="application/json" class="swiper-config">
+                  {
+                    "loop": true,
+                    "speed": 600,
+                    "autoplay": {
+                      "delay": 5000
+                    },
+                    "slidesPerView": "auto",
+                    "pagination": {
+                      "el": ".swiper-pagination",
+                      "type": "bullets",
+                      "clickable": true
+                    }
+                  }
+                </script>
+                            <div class="swiper-wrapper">
+
+                                <div class="swiper-slide">
+                                    <div class="testimonial-item">
+                                        <div class="d-flex">
+                                            <img src="assets/img/testimonials/testimonials-1.jpg"
+                                                class="testimonial-img flex-shrink-0" alt="">
+                                            <div>
+                                                <h3>Saul Goodman</h3>
+                                                <h4>Ceo &amp; Founder</h4>
+                                                <div class="stars">
+                                                    <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i
+                                                        class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i
+                                                        class="bi bi-star-fill"></i>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <p>
+                                            <i class="bi bi-quote quote-icon-left"></i>
+                                            <span>Proin iaculis purus consequat sem cure digni ssim donec porttitora
+                                                entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam
+                                                eget nibh et. Maecen aliquam, risus at semper.</span>
+                                            <i class="bi bi-quote quote-icon-right"></i>
+                                        </p>
+                                    </div>
+                                </div><!-- End testimonial item -->
+
+                                <div class="swiper-slide">
+                                    <div class="testimonial-item">
+                                        <div class="d-flex">
+                                            <img src="assets/img/testimonials/testimonials-2.jpg"
+                                                class="testimonial-img flex-shrink-0" alt="">
+                                            <div>
+                                                <h3>Sara Wilsson</h3>
+                                                <h4>Designer</h4>
+                                                <div class="stars">
+                                                    <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i
+                                                        class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i
+                                                        class="bi bi-star-fill"></i>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <p>
+                                            <i class="bi bi-quote quote-icon-left"></i>
+                                            <span>Export tempor illum tamen malis malis eram quae irure esse labore quem
+                                                cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua
+                                                noster fugiat irure amet legam anim culpa.</span>
+                                            <i class="bi bi-quote quote-icon-right"></i>
+                                        </p>
+                                    </div>
+                                </div><!-- End testimonial item -->
+
+                                <div class="swiper-slide">
+                                    <div class="testimonial-item">
+                                        <div class="d-flex">
+                                            <img src="assets/img/testimonials/testimonials-3.jpg"
+                                                class="testimonial-img flex-shrink-0" alt="">
+                                            <div>
+                                                <h3>Jena Karlis</h3>
+                                                <h4>Store Owner</h4>
+                                                <div class="stars">
+                                                    <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i
+                                                        class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i
+                                                        class="bi bi-star-fill"></i>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <p>
+                                            <i class="bi bi-quote quote-icon-left"></i>
+                                            <span>Enim nisi quem export duis labore cillum quae magna enim sint quorum
+                                                nulla quem veniam duis minim tempor labore quem eram duis noster aute
+                                                amet eram fore quis sint minim.</span>
+                                            <i class="bi bi-quote quote-icon-right"></i>
+                                        </p>
+                                    </div>
+                                </div><!-- End testimonial item -->
+
+                                <div class="swiper-slide">
+                                    <div class="testimonial-item">
+                                        <div class="d-flex">
+                                            <img src="assets/img/testimonials/testimonials-4.jpg"
+                                                class="testimonial-img flex-shrink-0" alt="">
+                                            <div>
+                                                <h3>Matt Brandon</h3>
+                                                <h4>Freelancer</h4>
+                                                <div class="stars">
+                                                    <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i
+                                                        class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i
+                                                        class="bi bi-star-fill"></i>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <p>
+                                            <i class="bi bi-quote quote-icon-left"></i>
+                                            <span>Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos
+                                                export minim fugiat minim velit minim dolor enim duis veniam ipsum anim
+                                                magna sunt elit fore quem dolore labore illum veniam.</span>
+                                            <i class="bi bi-quote quote-icon-right"></i>
+                                        </p>
+                                    </div>
+                                </div><!-- End testimonial item -->
+
+                                <div class="swiper-slide">
+                                    <div class="testimonial-item">
+                                        <div class="d-flex">
+                                            <img src="assets/img/testimonials/testimonials-5.jpg"
+                                                class="testimonial-img flex-shrink-0" alt="">
+                                            <div>
+                                                <h3>John Larson</h3>
+                                                <h4>Entrepreneur</h4>
+                                                <div class="stars">
+                                                    <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i
+                                                        class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i
+                                                        class="bi bi-star-fill"></i>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <p>
+                                            <i class="bi bi-quote quote-icon-left"></i>
+                                            <span>Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam
+                                                tempor noster veniam enim culpa labore duis sunt culpa nulla illum
+                                                cillum fugiat legam esse veniam culpa fore nisi cillum quid.</span>
+                                            <i class="bi bi-quote quote-icon-right"></i>
+                                        </p>
+                                    </div>
+                                </div><!-- End testimonial item -->
+
+                            </div>
+                            <div class="swiper-pagination"></div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </section><!-- /Testimonials Section -->
 
         <footer id="footer" class="footer light-background">
 
